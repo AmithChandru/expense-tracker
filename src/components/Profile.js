07568804ac188628aa1553cd65b44f03d29f9905
AmithChandru <amithc279@gmail.com>
2023-03-12 00:33:47 +0530

    Added user details

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './Profile.css';
 import LoginContext from './Store/LoginContext';
 
@@ -7,6 +7,33 @@ const Profile = () => {
   const [name, setName] = useState(null);
   const [url, setUrl] = useState(null);
   const loginCtx = useContext(LoginContext);
+
+  useEffect(() => {
+    getUserDetails();
+  }, [])
+
+  const getUserDetails = async () => {
+    await fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCszasJf5BQUdZzWYKXbjjvKo5BlnvW79Q', {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: loginCtx.token.idToken
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          data.users.forEach((item) => {
+            setName(item.displayName);
+            setUrl(item.photoUrl);
+          })
+        })
+      } else {
+        console.log(res);
+      }
+    })
+  }
 
   const handleUpdateClick = async () => {
     if (name && url) {
@@ -56,9 +83,9 @@ const Profile = () => {
         </section>
         <form className='ProfileForm'>
           <label>Full Name:</label>
-          <input type='text' onChange={(e) => setName(e.target.value)}/>
+          <input type='text' onChange={(e) => setName(e.target.value)} value={name} />
           <label>Profile Photo URL:</label>
-          <input type='text' onChange={(e) => setUrl(e.target.value)}/>
+          <input type='text' onChange={(e) => setUrl(e.target.value)} value={url} />
         </form>
         <button className='UpdateButton' onClick={handleUpdateClick}>Update</button>
       </div>
