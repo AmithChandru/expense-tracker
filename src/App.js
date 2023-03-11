9f5d380457bf19480610b2a6847213a4da94efb2
AmithChandru <amithc279@gmail.com>
2023-03-11 18:31:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="HeaderContainer">
+      <span className='TitleText'>MyWebLink</span>
+      <span className='NavText'>Home</span>
+      <span className='NavText'>Products</span>
+      <span className='NavText'>About Us</span>
+    </div>
+  )
+}
+
+export default Header;
