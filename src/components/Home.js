bd7e6f40421a671ca208649240bf627e5e6674e9
AmithChandru <amithc279@gmail.com>
2023-03-12 14:16:42 +0530

    Added delete and update

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,26 +8,31 @@ const Home = () => {
   const navigate = useNavigate();
   const loginCtx = useContext(LoginContext);
   const [items, setItems] = useState([]);
-  const [money, setMoney] = useState(null);
-  const [desc, setDesc] = useState(null);
-  const [category, setCategory] = useState(null);
+  const [money, setMoney] = useState('');
+  const [desc, setDesc] = useState('');
+  const [category, setCategory] = useState('');
 
   useEffect(() => {
-    fetch('https://react-movies-8029a-default-rtdb.asia-southeast1.firebasedatabase.app/expense.json')
-      .then((res) => {
-        let temp = [];
-        res.json().then((data) => {
-          for (const key in data) {
-            temp.push({
-              money: data[key].money,
-              desc: data[key].desc,
-              category: data[key].category
-            })
-          }
-        })
+    getItems();
+  }, [money, desc, category])
+
+  const getItems = async () => {
+    await fetch('https://react-movies-8029a-default-rtdb.asia-southeast1.firebasedatabase.app/expense.json')
+    .then((res) => {
+      let temp = [];
+      res.json().then((data) => {
+        for (const key in data) {
+          temp.push({
+            id: key,
+            money: data[key].money,
+            desc: data[key].desc,
+            category: data[key].category
+          })
+        }
         setItems(temp);
       })
-  })
+    })
+  }
 
   const handleClick = () => {
     navigate('/Profile');
@@ -57,6 +62,46 @@ const Home = () => {
     })
   }
 
+  const handleDelete = async (item) => {
+    await fetch(`https://react-movies-8029a-default-rtdb.asia-southeast1.firebasedatabase.app/expense/${item.id}.json`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          console.log(data);
+          getItems();
+        })
+      } else {
+        res.json().then((data) => {
+          console.log(data.error.message);
+        })
+      }
+    })
+  }
+
+  const handleEdit = async (item) => {
+    handleDelete(item);
+    setMoney(item.money);
+    setDesc(item.desc);
+    setCategory(item.category);
+    await fetch(`https://react-movies-8029a-default-rtdb.asia-southeast1.firebasedatabase.app/expense/${item.id}.json`, {
+      method: 'POST',
+      body: JSON.stringify({
+        money: money,
+        desc: desc,
+        category: category
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => {
+      console.log(res);
+    })
+  }
+
   const handleAddExpense = async () => {
     await fetch('https://react-movies-8029a-default-rtdb.asia-southeast1.firebasedatabase.app/expense.json', {
       method: 'POST',
@@ -72,6 +117,7 @@ const Home = () => {
         if (res.ok) {
           res.json().then((data) => {
             console.log(data);
+            getItems();
           })
         } else {
           res.json().then((data) => {
@@ -79,14 +125,14 @@ const Home = () => {
           })
         }
     })
-    setItems((state) => [
+    /* setItems((state) => [
       ...state,
       {
         money: money,
         desc: desc,
         category: category
       }
-    ]);
+    ]); */
   }
 
   return (
@@ -101,9 +147,9 @@ const Home = () => {
       </div>
       <div className='ExpensesContainer'>
         <section className='InputContainer'>
-          <input placeholder='Money Spent' onChange={(e) => setMoney(e.target.value)} />
-          <input placeholder='Description' onChange={(e) => setDesc(e.target.value)} />
-          <select onChange={(e) => setCategory(e.target.value)}>
+          <input placeholder='Money Spent' onChange={(e) => setMoney(e.target.value)} value={money} />
+          <input placeholder='Description' onChange={(e) => setDesc(e.target.value)} value={desc} />
+          <select onChange={(e) => setCategory(e.target.value)} value={category} >
             <option value="food">Food</option>
             <option value="fuel">Fuel</option>
             <option value="others">Others</option>
@@ -111,11 +157,14 @@ const Home = () => {
         </section>
         <button onClick={handleAddExpense}>Add Expense</button>
         {items.map((item) => {
+          console.log(item);
           return (
             <div>
               <span>{item.money}</span>
               <span>{item.desc}</span>
               <span>{item.category}</span>
+              <button style={{margin: '0px 10px'}} onClick={() => handleEdit(item)}>Edit</button>
+              <button onClick={() => handleDelete(item)}>Delete</button>
             </div>
           )
         })}
